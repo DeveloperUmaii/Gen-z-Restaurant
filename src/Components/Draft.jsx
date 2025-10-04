ba00e8d1f59a1a25eea566425ff4303d5613ad5c
uamii <umayarhossainashik48889@gmail.com>
2025-10-04 17:10:54 +0600

    Chef Recomended Pagination Done

---
+++ src/Components/Draft.jsx
@@ -0,0 +1,3 @@
+git add .
+git commit -m"Chef Recomended Pagination Done"
+git push
--- src/Pages/HomeComponents/ChefRecomended.jsx
+++ src/Pages/HomeComponents/ChefRecomended.jsx
@@ -4,30 +4,50 @@ import ChefRecomendedCard from "./ChefRecomendedCard";
 import useMenu from "../../hooks/useMenu";
 
 //swiper import
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
 
 const ChefRecomended = () => {
   const [menu] = useMenu();
 
+  const itemsPerPage = 8;
+  const slideMenu = [];
+
+  for (let i = 0; i < menu.length; i += itemsPerPage) {
+    slideMenu.push(menu.slice(i, i + itemsPerPage));
+  }
+
   const pagination = {
     clickable: true,
     renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + '</span>';
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
     },
   };
 
   return (
     <div>
       <SecTionTitle subHeading="Should Try" heading="Chef Recommendes" />
-      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
-        {menu.map((ChefReciepe) => (
-          <ChefRecomendedCard key={ChefReciepe?._id} ChefCard={ChefReciepe} />
+
+      <Swiper
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {slideMenu.map((items, index) => (
+          <SwiperSlide key={index}>
+            <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
+              {items.map((ChefReciepe) => (
+                <ChefRecomendedCard
+                  key={ChefReciepe?._id}
+                  ChefCard={ChefReciepe}
+                />
+              ))}
+            </div>
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </div>
   );
 };
