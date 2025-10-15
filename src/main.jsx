3716e09ab43db0d676eed36e3dabbd6bdecc9a30
uamii <umayarhossainashik48889@gmail.com>
2025-10-15 10:28:27 +0600

    Authcontext

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'  // ইমপোর্ট না করলে টেল উইন্ড কাজ করবে না ইনডেক্স সিএসএস
-import { RouterProvider } from 'react-router-dom'
-import router from './Router/Router';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"; // ইমপোর্ট না করলে টেল উইন্ড কাজ করবে না ইনডেক্স সিএসএস
+import { RouterProvider } from "react-router-dom";
+import router from "./Router/Router";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./providers/Authprovider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode >
-    <HelmetProvider>
-      <Helmet  defaultTitle='Gen-Z_Resturant'  />
-        <RouterProvider router={router} />
-    </HelmetProvider>
-  </ React.StrictMode >
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>  {/*Sompurno web app take ei context diye cover korlam*/}
+          <HelmetProvider>
+            <Helmet defaultTitle="Gen-Z_Resturant" />
+            <RouterProvider router={router} />
+          </HelmetProvider>
+    </AuthProvider>
+  </React.StrictMode>
 );
---
+++ src/providers/Authprovider.jsx
@@ -0,0 +1,66 @@
+
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase/firebase.config";
+
+export const AuthContext = createContext(null)
+
+const googleProvider = new GoogleAuthProvider();
+  export  const ParentContextHook =useContext(AuthContext);
+
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null)
+
+    const registrationUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+
+    }
+
+    const logInUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+
+    }
+    const googlelogIn = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const logOut = () => {
+       return signOut(auth)
+    }
+
+    const contextInfo = {
+        registrationUser,
+        logInUser,
+        user,
+        setUser,
+        googlelogIn,
+        logOut,
+
+    }
+
+    useEffect(() => {
+       const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                setUser(currentUser)
+                console.log(currentUser);
+            } else {
+                     setUser(currentUser);
+                     console.log('log out use Effect')
+            }
+                });
+                return () => {
+                    unSubscribe()
+                }
+    }, [])
+
+    return (
+            <div>
+            <AuthContext.Provider value={contextInfo}>
+                {children}
+            </AuthContext.Provider>
+            </div>
+    );
+};
+
+export default AuthProvider;
