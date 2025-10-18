77cce3574ccf56911cfbf6e440b5efb3bbd9717d
uamii <umayarhossainashik48889@gmail.com>
2025-10-18 23:10:15 +0600

    today

--- src/Components/Draft.jsx
+++ src/Components/Draft.jsx
@@ -28,6 +28,22 @@ git push
             height="20"
             fill="none"
             stroke="currentColor"
+            
+            
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-eye"
+          >
+            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+            <circle cx="12" cy="12" r="3" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="none"
+            stroke="currentColor"
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
