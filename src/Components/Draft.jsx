4e945f6b44b95d9b09d6fd137367b26105d4d020
uamii <umayarhossainashik48889@gmail.com>
2025-10-27 15:23:00 +0600

    d

--- src/Components/Draft.jsx
+++ src/Components/Draft.jsx
@@ -5,10 +5,9 @@ const Draft = () => {
         <div>
             <h1>write something</h1>
             <h1>write something aroraa</h1>
-            <h1>write something aroraa</h1>
-            <h1>write something aroraa</h1>
-            <h1>write something aroraa</h1>
-            <h1>write something aroraa</h1>
+            <h1> something aroraa</h1>
+            <h2 className="">';lkjhgfds</h2>
+         
         </div>
     );
 };
