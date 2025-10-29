9c1dd1cef480acb9c41c4d64da4847c43dbd6722
uamii <umayarhossainashik48889@gmail.com>
2025-10-29 09:51:44 +0600

    Start 2D

--- src/Components/Draft.jsx
+++ src/Components/Draft.jsx
@@ -3,12 +3,7 @@ import React from 'react';
 const Draft = () => {
     return (
         <div>
-            <h1>write something</h1>
-            <h1>write something aroraa</h1>
-            <h1> something aroraa</h1>
-            <h2 className="">';lkjhgfds</h2>
-            <div className=""><h5>ggggggggg</h5></div>
-         
+            <h1>write something</h1>         
         </div>
     );
 };
