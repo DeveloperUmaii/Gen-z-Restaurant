c0521b24129d9de0c495ac03d63499a18aeca80d
uamii <umayarhossainashik48889@gmail.com>
2026-01-13 15:13:55 +0600

    draft Jwt NOTE

--- src/Components/Draft.jsx
+++ src/Components/Draft.jsx
@@ -92,6 +92,41 @@ Step 10.All user theke header(Comment kora tuku) kete deoa
       // }
     );
 Step 11. Axios secure a response
+
+  useEffect(() => {
+    // request interceptor
+    const reqInterceptor = backEndServerLink.interceptors.request.use(
+      (config) => {
+        const token = localStorage.getItem("access-token");
+        console.log('Request stop bye Interceptors :_',token)
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+      }
+    );
+
+    // response interceptor
+    const resInterceptor = backEndServerLink.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const status = error.response?.status;
+        console.log('status error in the INTERCEPTORS',status)
+        if (status === 401 || status === 403) {
+          await logOut();
+          navigate("/login");
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    // cleanup (VERY IMPORTANT)
+    return () => {
+      backEndServerLink.interceptors.request.eject(reqInterceptor);
+      backEndServerLink.interceptors.response.eject(resInterceptor);
+    };
+  }, [logOut, navigate]);
+
+
+// eituku bad 
           backEndServerLink.interceptors.response.use(function(response) {
               return response;
             }, (error) => {
@@ -99,5 +134,8 @@ Step 11. Axios secure a response
               console.log('status error in the INTERCEPTORS',status)
               return Promise.reject(error);
             });
+
+
+
 Step 12.
 Step 13.
