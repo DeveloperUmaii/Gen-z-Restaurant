b82bcf854a05ff9f39ca2d92b9fc7c353d394db5
uamii <umayarhossainashik48889@gmail.com>
2026-01-13 09:03:25 +0600

    demo 13-01-2025

--- src/hooks/hookAxiosSecure.jsx
+++ src/hooks/hookAxiosSecure.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
+import UseAuthHook from "../providers/ContexHook/UseAuthHook";
 
 export const backEndServerLink = axios.create ({
      baseURL:'http://localhost:5000' })
      
-      const hookAxiosSecure = () => {
+    const hookAxiosSecure = () => {
       const navigate = useNavigate()
+      const {logOut} = UseAuthHook()
   // Add a request interceptor
 backEndServerLink.interceptors.request.use(function (config) {
           const token = localStorage.getItem('access-token')
@@ -16,11 +18,12 @@ backEndServerLink.interceptors.request.use(function (config) {
 
 backEndServerLink.interceptors.response.use(function(response) {
     return response;
-  }, (error) => {
+  }, async(error) => {
     const status = error.response.status;
     console.log('status error in the INTERCEPTORS',status)
     if(status === 401 || status === 403 ){
-        navigate('/login')
+       await logOut();
+       navigate('/login')
     }
     return Promise.reject(error);
   });
