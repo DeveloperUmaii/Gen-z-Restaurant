a6ed56ad9890ba02918e9d3914eda45e7e328d73
uamii <umayarhossainashik48889@gmail.com>
2026-01-13 14:17:12 +0600

    interceptor err done

--- src/hooks/hookAxiosSecure.jsx
+++ src/hooks/hookAxiosSecure.jsx
@@ -1,32 +1,47 @@
 import axios from "axios";
 import UseAuthHook from "../providers/ContexHook/UseAuthHook";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-export const backEndServerLink = axios.create ({
-     baseURL:'http://localhost:5000' })
-     
-    const hookAxiosSecure = () => {
-      const navigate = useNavigate()
-      const {logOut} = UseAuthHook()
-  // Add a request interceptor
-backEndServerLink.interceptors.request.use(function (config) {
-          const token = localStorage.getItem('access-token')
-          console.log('Request stop bye Interceptors :_',token)
-          config.headers.authorization = `Bearer ${token}`
+export const backEndServerLink = axios.create({
+  baseURL: "http://localhost:5000",
+});
+
+const hookAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = UseAuthHook();
+
+  useEffect(() => {
+    // request interceptor
+    const reqInterceptor = backEndServerLink.interceptors.request.use(
+      (config) => {
+        const token = localStorage.getItem("access-token");
+        console.log('Request stop bye Interceptors :_',token)
+        config.headers.authorization = `Bearer ${token}`;
         return config;
-      }),   
-      function (error) { return Promise.reject(error);}
+      }
+    );
 
-backEndServerLink.interceptors.response.use(function(response) {
-    return response;
-  }, async(error) => {
-    const status = error.response.status;
-    console.log('status error in the INTERCEPTORS',status)
-    if(status === 401 || status === 403 ){
-       await logOut();
-       navigate('/login')
-    }
-    return Promise.reject(error);
-  });
+    // response interceptor
+    const resInterceptor = backEndServerLink.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const status = error.response?.status;
+        console.log('status error in the INTERCEPTORS',status)
+        if (status === 401 || status === 403) {
+          await logOut();
+          navigate("/login");
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    // cleanup (VERY IMPORTANT)
+    return () => {
+      backEndServerLink.interceptors.request.eject(reqInterceptor);
+      backEndServerLink.interceptors.response.eject(resInterceptor);
+    };
+  }, [logOut, navigate]);
 
   return backEndServerLink;
 };
